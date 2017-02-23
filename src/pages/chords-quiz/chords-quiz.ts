3db7a332c02c3ac43ed086a08a0aae1611dfe911
birkoss <admin@birkoss.com>
2017-02-23 07:01:39 -0500

    Quiz scaffolding

--- src/pages/chords-quiz/chords-quiz.ts
+++ src/pages/chords-quiz/chords-quiz.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { ConfigProvider } from '../../providers/config-provider';
+import { DataProvider } from '../../providers/data-provider';
+
+import { Note } from '../../classes/note';
+import { Type } from '../../classes/type';
+
 @Component({
   selector: 'chords-quiz',
   templateUrl: 'chords-quiz.html'
 })
 export class ChordsQuizPage {
+    current_note:Note;
+    current_type:Type;
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, public config:ConfigProvider, public data:DataProvider) {
+      this.current_note = data.getNote('A');
+      this.current_type = this.data.getType('Major', 'Triad');
   }
 
 }
