7ca80531f5a354447bd6cbe0234845afa7b93395
birkoss <admin@birkoss.com>
2017-02-26 12:14:00 -0500

    Finishing the quiz modal

--- src/modals/chords-quiz/chords-quiz.ts
+++ src/modals/chords-quiz/chords-quiz.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { ViewController } from 'ionic-angular';
+import { ViewController, ToastController } from 'ionic-angular';
 
 import { FavoritesProvider } from '../../providers/favorites-provider';
 import { ConfigProvider } from '../../providers/config-provider';
@@ -13,14 +13,37 @@ import { DataProvider } from '../../providers/data-provider';
 
 export class ChordsQuizModal {
 
-    constructor(public viewCtrl: ViewController, private data: DataProvider, private config: ConfigProvider, public favorites:FavoritesProvider) {
+    constructor(public viewCtrl: ViewController, private data: DataProvider, private config: ConfigProvider, public favorites:FavoritesProvider, public toastCtrl:ToastController) {
     }
 
     public close():void {
         this.viewCtrl.dismiss();
     }
 
-    public onFilterChanged():void {
+    public onFilterChanged(element, element_value):void {
+        let atLeastOne:Boolean = false;
+        for (let key in this.config.ChordsFilters['quiz_chord_types']) {
+            if (this.config.ChordsFilters['quiz_chord_types'][key]) {
+                atLeastOne = true;
+                break;
+            }
+        }
+        if (!atLeastOne) {
+            this.config.ChordsFilters['quiz_chord_types']['Major'] = true;
+
+            if (!element.checked) {
+                let toast = this.toastCtrl.create({
+                    message: 'Il doit y avoir au moins un type!',
+                    position: 'top',
+                    duration: 3000
+                });
+                toast.present();
+            }
+            if (element_value == 'Major') {
+                element.checked = true;
+            }
+        }
+
         this.config.save();
     }
 }
