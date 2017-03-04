8c9aa68c44cde012ae9ade20d4c6706d143ded7b
birkoss <admin@birkoss.com>
2017-03-04 16:00:06 -0500

    Changing the musical note, now based on the line

--- src/components/musical-note/musical-note.ts
+++ src/components/musical-note/musical-note.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 
+import { DataProvider } from '../../providers/data-provider';
+
 import { Note } from '../../classes/note';
 
 @Component({
@@ -10,12 +12,49 @@ export class MusicalNote {
     _note:Note;
     _direction:string;
 
+    clef:string = "G";
+    clefPosition:number = 5;
+
+    line:number = 0;
+    accidental:string = "";
+
     @Input('note')
     set note(note:Note) {
         this._note = note;
     }
 
-    constructor() { }
+    constructor(public data:DataProvider) { }
+
+    ngOnInit() {
+        let clefIndex:number = 0;
+        let noteIndex:number = 0;
+
+        let letters:Array<Object> = this.data.getLetters();
+        for (let i:number=0; i<letters.length; i++) {
+            if (letters[i]['name'] == this.clef) {
+                clefIndex = i;
+            }
+            if (letters[i]['name'] == this._note.letter['name']) {
+                noteIndex = i;
+            }
+        }
+
+
+        let differenceIndex:number = clefIndex - noteIndex;
+        
+        this.line = this.clefPosition - differenceIndex;
+
+        if (this._note.accidental < 0) {
+            this.accidental = 'flat';
+        } else if (this._note.accidental > 0) {
+            this.accidental = 'sharp';
+        }
+
+        console.log(this._note.letter);
+        console.log(noteIndex + "/" + clefIndex + "=" + differenceIndex);
+        console.log('Line:' + this.line);
+        console.log("--------------------------------");
+    }
 
     getClasses() {
         let classes: Array<string> = [];
