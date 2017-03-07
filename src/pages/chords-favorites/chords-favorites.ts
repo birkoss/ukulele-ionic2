f61d21a1ba06f83d454b9ec48b1264a2a48dbddb
birkoss <admin@birkoss.com>
2017-03-07 15:24:27 -0500

    Chords favorites

--- src/pages/chords-favorites/chords-favorites.ts
+++ src/pages/chords-favorites/chords-favorites.ts
@@ -4,33 +4,55 @@ import { NavController, PopoverController } from 'ionic-angular';
 
 import { ChordsDetailPage } from '../chords-detail/chords-detail';
 
-import { ChordsOptionsPopover } from '../../popovers/chords-options/chords-options';
+import { GeneralPopover } from '../../popovers/general/general';
 
 import { ConfigProvider } from '../../providers/config-provider';
 import { DataProvider } from '../../providers/data-provider';
 import { FavoritesProvider } from '../../providers/favorites-provider';
 
 import { Chord } from '../../classes/chord';
+import { Note } from '../../classes/note';
 import { Position } from '../../classes/Position';
 
 @Component({
-  selector: 'chords-favorites',
-  templateUrl: 'chords-favorites.html'
+    selector: 'chords-favorites',
+    templateUrl: 'chords-favorites.html'
 })
 export class ChordsFavoritesPage {
+    chords:Array<Object> = [];
 
-  constructor(public navCtrl: NavController, public dataProvider: DataProvider, private config: ConfigProvider, private favorites: FavoritesProvider, public popoverCtrl: PopoverController) {
-    
-  }
+    constructor(public navCtrl:NavController, public data:DataProvider, private config:ConfigProvider, private favorites:FavoritesProvider, public popoverCtrl:PopoverController) { }
 
-  public showPopup(event, type:string) {
-      let popover = this.popoverCtrl.create(ChordsOptionsPopover);
-      popover.present({
-          ev: event
-      });
-  }
+    ionViewDidEnter() {
+        this.generateList();
+    }
 
-  public showDetail(chord:Object): void {
-      this.navCtrl.push(ChordsDetailPage, {note: chord['note'], type:chord['type'], 'position':chord['position']});
-  }
+    showPopup(event) {
+        let popover = this.popoverCtrl.create(GeneralPopover);
+        popover.present({
+            ev: event
+        });
+    }
+
+    generateList() {
+        this.chords = [];
+
+        this.favorites.all('chords').forEach(favorite => {
+            let note:Note = this.data.getNote(favorite['letter'], favorite['accidental']);
+            let form:Object = this.data.getForm(favorite['type'], favorite['quality']);
+
+            let chord:Chord = this.data.getChord(note, form);
+
+            this.chords.push({
+                note:note,
+                form:form,
+                position:chord.positions[favorite['position']]
+            });
+        });
+    }
+
+    showDetail(chord:Object) {
+        console.log(chord);
+        this.navCtrl.push(ChordsDetailPage, {chord: chord['chord']});
+    }
 }
